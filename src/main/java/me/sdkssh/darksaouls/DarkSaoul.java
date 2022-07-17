8fd9efca08d05cf0687c5b95d019284024e70aa2
SDKSSH <sdk_ssh@protonmail.com>
2022-07-17 12:12:32 +0200

    Init listener on deathSaoul

--- src/main/java/me/sdkssh/darksaouls/DarkSaoul.java
+++ src/main/java/me/sdkssh/darksaouls/DarkSaoul.java
@@ -1,10 +1,12 @@
 package me.sdkssh.darksaouls;
 
+import me.sdkssh.darksaouls.events.DeathListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DarkSaoul extends JavaPlugin {
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new DeathListener(), this);
         super.onEnable();
     }
 }
