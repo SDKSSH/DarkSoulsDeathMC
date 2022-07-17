a31dc244344ee401d2664bf619125dd58363b24c
SDKSSH <sdk_ssh@protonmail.com>
2022-07-17 11:46:37 +0200

    Respawn on death

--- src/main/java/me/sdkssh/darksaouls/events/DeathListener.java
+++ src/main/java/me/sdkssh/darksaouls/events/DeathListener.java
@@ -1,7 +1,12 @@
 package me.sdkssh.darksaouls.events;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DeathListener implements Listener {
-
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e){
+        e.getEntity().spigot().respawn();
+    }
 }
